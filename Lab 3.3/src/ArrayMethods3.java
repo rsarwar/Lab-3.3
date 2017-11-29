82d98c2fdf714b7b5c7688da2ab414403e8d454f
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2017-11-29 13:04:05 -0500

    bubbleSort

--- Lab 3.3/src/ArrayMethods3.java
+++ Lab 3.3/src/ArrayMethods3.java
@@ -1,9 +1,14 @@
-
+/*
+ * Miriam Monroe and Ramisha Sarwar
+ * Lab 3.3
+ */
 public class ArrayMethods3 {
 	public static void main (String [] args)
 	{
-		int[] nums = {2,3,8,5,4};
-		insertionSort(nums);
+		int[] nums = {3,2,8,5,4};
+		//insertionSort(nums);
+		//selectionSort(nums);
+		bubbleSort(nums);
 		printArr(nums);
 	}
 	public static void insertionSort(int[] list1)
@@ -23,21 +28,46 @@ public class ArrayMethods3 {
 	}
 	public static void selectionSort(int[] list1)
 	{
-		int num = list1[0];
-		//int front = 
-		for(int i = 0; i < list1.length; i++)
+		int temp = 0;
+		for(int i = 0; i < list1.length-1; i++)
 		{
-			if(list1[i] < num)
+			int num = i;
+			for(int j = i+1; j < list1.length; j++)
 			{
-				list1[i] = 
-				num = list1[i];
+				if(list1[j] < list1[num])
+				{
+					num = j;
+				}
+			}
+
+			swap(list1, num, i);
+		}
+
+	}
+	
+
+	
+	public static void bubbleSort(int[] list1)
+	{
+		for(int i = 0; i < list1.length-1; i++)
+		{
+			for(int j = 0; j < list1.length-i-1; j++)
+			{
+				if(list1[j+1] < list1[j])
+				{
+					swap(list1, j, j+1);
+				}
 			}
 		}
 	}
-	public static void bubbleSort(int[] list1)
+	
+	public static void swap(int[] arr, int i, int j)
 	{
-		
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
 	}
+	
 	public static void printArr(int[] list)
 	{
 		for(int i = 0; i<list.length; i++)
